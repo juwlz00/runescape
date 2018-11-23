3d72c2201755a1bc4370240863cd23819d4a7c8c
Julian Chan <j.chan0506@gmail.com>
2018-11-23 10:08:06 -0800

    fetching json fixed

--- runescape.pl
+++ runescape.pl
@@ -1,13 +1,13 @@
-
-
 :- use_module(library(http/http_open)).
 :- use_module(library(http/json)).
 :- use_module(library(http/json_convert)).
+:- dynamic(found/2).
+% :- dynamic(similarDifficulty/2).
 
 % Make a call to the Game API and build the knowledge base dynamically
 steam(Dict, R, S) :-
   setup_call_cleanup(
-    http_open('https://api.myjson.com/bins/ymjhu', In, []),
+    http_open('https://api.myjson.com/bins/zc1s6', In, []),
     json_read_dict(In, Dict),
     close(In)
   ),
@@ -18,17 +18,22 @@ steam(Dict, R, S) :-
 dict_x(X, X.apps).
 dict_y([H|R], H) :-
   % Remove the quotations from the JSON values
-  term_to_atom(name,H.name),
-  term_to_atom(difficulty,H.difficulty),
-  term_to_atom(length,H.length),
-  term_to_atom(questpoints,H.qp),
+  term_to_atom(Name,H.name),
+  term_to_atom(Difficulty,H.difficulty),
+  term_to_atom(Length,H.length),
+  term_to_atom(Qp,H.questpoints),
   % Build knowledge base
-  assert(quest(name)),
-  assert(quest_difficulty(name,difficulty)),
-  assert(quest_length(name,length)),
-  assert(quest_points(name,questpoints)),
+  assert(quest(Name)),
+  assert(quest_difficulty(Name,Difficulty)),
+  assert(quest_length(Name,Length)),
+  assert(quest_points(Name,Qp)),
   dict_y(R, _).
 
 % Build the knowledge base first whenever the project is made before allowing
 % users to make queries
 %:- initialization(steam(_,_,_)).
+
+% Helper functions to complete specific queries
+% similarDifficulty(X,Y) :-
+%  setof((X,Y), B^(quest_difficulty(X,B), quest_difficulty(Y,B), \+X=Y), &&Quest),
+%  member((X,Y), Quest).
