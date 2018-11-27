95a72b044e906c760e1b9cb086d4f447a316636d
mitchsc <mitchellscurrie@gmail.com>
2018-11-26 23:15:49 -0800

    quest req

--- runescape.pl
+++ runescape.pl
@@ -1,43 +1,9 @@
-:- use_module(library(http/http_open)).
-:- use_module(library(http/json)).
-:- use_module(library(http/json_convert)).
-
-
-% Make a call to the Game API and build the knowledge base dynamically
-steam(Dict, R, S) :-
-  setup_call_cleanup(
-    http_open('https://api.myjson.com/bins/zc1s6', In, []),
-    json_read_dict(In, Dict),
-    close(In)
-  ),
-  dict_x(Dict, R),
-  dict_y(R,S).
-
-% Parse the JSON file retrieved from the API call and build knowledge base dynamically
-dict_x(X, X.apps).
-dict_y([H|R], H) :-
-  % Remove the quotations from the JSON values
-  term_to_atom(Name,H.name),
-  term_to_atom(Difficulty,H.difficulty),
-  term_to_atom(Length,H.length),
-  term_to_atom(Qp,H.questpoints),
-  % Build knowledge base
-  assert(quest(Name)),
-  assert(quest_difficulty(Name,Difficulty)),
-  assert(quest_length(Name,Length)),
-  assert(quest_points(Name,Qp)),
-  dict_y(R, _).
-
-% Build the knowledge base first whenever the project is made before allowing users to make queries
-:- initialization(steam(_,_,_)).
-:- initialization(start(A)).
-
 start(Ans) :-
     write("What level are you?: "),flush_output(current_output),
-    readln(Lnlvl),
+    readln([Head|Lnlvl]),
 	write("Are you a member?: y/n "),flush_output(current_output),
     readln(Lnmem),
-	assert(user_level(Lnlvl)),
+	assert(user_level(Head)),
 	assert(user_member(Lnmem)).
 
 % NLP based off Dr.Poole's geography.pl code
@@ -78,7 +44,9 @@ noun([quest | T],T,Obj) :- quest(Obj).
 noun([X | T],T,X) :- quest(X).
 noun([X | T],T,X) :- quest_difficulty(A,X).
 
-reln([place,holder | T],T,O1,O2) :- quest_difficulty(O1,O2).
+reln([unlocked, by | T],T,O1,O2) :- proceeds(O1,O2).
+reln([required, for | T],T,O1,O2) :- preceeds(O1,O2).
+
 
 % NLP v2
 
@@ -190,11 +158,55 @@ q(Ans) :-
     question(Ln,End,Ans),
     member(End,[[],['?'],['.']]).
 	
+quest(cooks_assistant).
+quest(dragon_slayer).
+quest(demon_slayer).
+quest(the_knights_sword).
+quest(black_knights_fortress).
+
+difficulty(novice).
+difficulty(intermediate).
+difficulty(experienced).
+
+quest_difficulty(cooks_assistant,novice).
+quest_difficulty(dragon_slayer,intermediate).
+quest_difficulty(demon_slayer,novice).
+quest_difficulty(the_knights_sword,intermediate).
+quest_difficulty(black_knights_fortress,novice).
+
+length(short).
+length(long).
+length(medium).
+
+quest_length(cooks_assistant,short).
+quest_length(dragon_slayer,long).
+quest_length(demon_slayer,medium).
+quest_length(the_knights_sword,medium).
+quest_length(black_knights_fortress,short).
+
+quest_points(cooks_assistant,1).
+quest_points(dragon_slayer,2).
+quest_points(demon_slayer,3).
+quest_points(the_knights_sword,1).
+quest_points(black_knights_fortress,3).
+
+preceeds(cooks_assistant,demon_slayer).
+preceeds(cooks_assistant,dragon_slayer).
+preceeds(demon_slayer,dragon_slayer).
+preceeds(dragon_slayer,the_knights_sword).
+preceeds(cooks_assistant,the_knights_sword).
+preceeds(demon_slayer,the_knights_sword).
+
+proceeds(B,A):-
+	preceeds(A,B).
+	
 % Try
 % ?- ask(['What',is,a,quest],A).
 % ?- ask(['What',is,a,novice,difficulty,quest],A).
 % ?- ask(['What',is,a,medium,length,quest],A).
 % ?- ask(['What',is,a,novice,difficulty,short,length,quest],A).
+% ?- ask(['What',is,a,novice,difficulty,quest,required,for,demon_slayer],A).
+̀% ?- ask(['What',is,a,quest,unlocked,by,demon_slayer],A).
 % ?- ask2(['What',is,a,quest,with,similar,difficulty,as,demon_slayer],A).
 % ?- ask2(['What',is,a,quest,with,the,same,difficulty,as,demon_slayer],A).
 % ?- ask2(['What',is,a,quest,with,similar,length,as,cooks_assistant],A).
